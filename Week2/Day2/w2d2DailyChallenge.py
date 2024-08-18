19d8dcd8ed1e61a9780a90ec86aa8b9e01f2f5a2
Shira-Code <Shira.code@gmail.com>
2024-08-18 17:27:48 +0300

    w2d2 xp and challenge

--- Week2/Day2/w2d2DailyChallenge.py
+++ Week2/Day2/w2d2DailyChallenge.py
@@ -52,3 +52,63 @@
 # The methods used to change page should be chainable, so you can call them one after the other like this: p.nextPage().nextPage()
 # Please set the p.totalPages and p.currentPage attributes to the appropriate number as there cannot be a page 0.
 # If a page is outside of the totalPages attribute, then the goToPage method should go to the closest page to the number provided (e.g. there are only 5 total pages, but p.goToPage(10) is given: the p.currentPage should be set to 5; if 0 or a negative number is given, p.currentPage should be set to 1).
+
+# class Pagination:
+#     def __init__(self, items=None, page_size=10):
+#         self.items = items if items is not None else []
+#         self.page_size = page_size
+#         self.current_page = 1
+
+#     def get_total_pages(self):
+       
+#         return (len(self.items) + self.page_size - 1) // self.page_size
+
+#     def get_visible_items(self):
+       
+#         start_index = (self.current_page - 1) * self.page_size
+#         end_index = min(start_index + self.page_size, len(self.items))
+#         return self.items[start_index:end_index]
+
+#     def prev_page(self):
+       
+#         if self.current_page > 1:
+#             self.current_page -= 1
+#         return self.get_visible_items()
+
+#     def next_page(self):
+        
+#         if self.current_page < self.get_total_pages():
+#             self.current_page += 1
+#         return self.get_visible_items()
+
+#     def first_page(self):
+       
+#         self.current_page = 1
+#         return self.get_visible_items()
+
+#     def last_page(self):
+        
+#         self.current_page = self.get_total_pages()
+#         return self.get_visible_items()
+
+#     def go_to_page(self, page_num):
+       
+#         if 1 <= page_num <= self.get_total_pages():
+#             self.current_page = page_num
+#         else:
+#             raise ValueError("Page number out of range")
+#         return self.get_visible_items()
+
+
+# if __name__ == "__main__":
+#     alphabet_list = list("abcdefghijklmnopqrstuvwxyz")
+#     p = Pagination(alphabet_list, 4)
+
+#     print("Initial page:", p.get_visible_items())
+#     print("Next page:", p.next_page())
+#     print("Next page:", p.next_page())
+#     print("Previous page:", p.prev_page())
+#     print("First page:", p.first_page())
+#     print("Last page:", p.last_page())
+#     print("Go to page 3:", p.go_to_page(3))
+#     print("Go to page 1:", p.go_to_page(1))
