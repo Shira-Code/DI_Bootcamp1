680f4d49aeea6e4ebe2576b6cd77f152b99e8dc5
Shira-Code <Shira.code@gmail.com>
2024-08-18 08:40:10 +0300

    8/18 8:40

--- Week2/Day5/w2D5MiniProjectAnagram.py
+++ Week2/Day5/w2D5MiniProjectAnagram.py
@@ -28,12 +28,12 @@
 
 # class AnagramChecker:
 #     def __init__(self, word_list_file: str):
-#         """Initializes the AnagramChecker with a word list from the given file."""
+#        
 #         self.word_list = set()
 #         self.load_word_list(word_list_file)
 
 #     def load_word_list(self, filename: str) -> None:
-#         """Loads words from the specified file into a set."""
+#        
 #         try:
 #             with open(filename, 'r') as file:
 #                 self.word_list = set(line.strip().lower() for line in file)
@@ -43,17 +43,17 @@
 #             raise IOError(f"Error reading the file {filename}.")
 
 #     def is_valid_word(self, word: str) -> bool:
-#         """Checks if the given word is in the word list."""
+#         
 #         return word.lower() in self.word_list
 
 #     def get_anagrams(self, word: str) -> list[str]:
-#         """Finds all anagrams of the given word from the word list."""
+#       
 #         word = word.lower()
 #         anagrams = [w for w in self.word_list if self.is_anagram(word, w) and w != word]
 #         return sorted(anagrams)
 
 #     def is_anagram(self, word1: str, word2: str) -> bool:
-#         """Checks if two words are anagrams."""
+#        
 #         return sorted(word1.lower()) == sorted(word2.lower())
     
 
