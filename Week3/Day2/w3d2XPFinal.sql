f90e4d00097626d348cffb671ee293801fd555a3
Shira-Code <Shira.code@gmail.com>
2024-08-19 18:49:25 +0300

    w3d2 xp final

--- Week3/Day2/w3d2XPFinal.sql
+++ Week3/Day2/w3d2XPFinal.sql
@@ -1,3 +1,28 @@
+-- Exercise 1
+
+-- We will work on the public database that we created yesterday.
+-- Use SQL to get the following from the database:
+-- All items, ordered by price (lowest to highest).
+SELECT *
+FROM items
+ORDER BY item_price ASC;
+-- Items with a price above 80 (80 included), ordered by price (highest to lowest).
+
+SELECT *
+FROM items
+WHERE item_price >= 80
+ORDER BY item_price DESC;
+-- The first 3 customers in alphabetical order of the first name (A-Z) – exclude the primary key column from the results.
+select * from customers;
+SELECT customer_first_name, customer_last_name
+FROM customers
+ORDER BY customer_first_name ASC
+LIMIT 3;
+-- All last names (no other columns!), in reverse alphabetical order (Z-A)
+SELECT customer_last_name
+FROM customers
+ORDER BY customer_last_name DESC;
+
 -- 1. In the dvdrental database write a query to select all the columns from the “customer” table.
 
 SELECT * FROM customer;
@@ -49,7 +74,6 @@ select * from inventory;
 -- inventory_id, film_id
 -- select * from film;
 	-- film_id
-
 
 SELECT *
 FROM film
