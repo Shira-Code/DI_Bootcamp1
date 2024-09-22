49634b143877b6134d2e4237c62f6304bc690095
Shira-Code <Shira.code@gmail.com>
2024-09-22 23:02:38 +0300

    w8d1 challenge

---
+++ Week8/Day1/w8d1Challenge.js
@@ -0,0 +1,79 @@
+// Daily challenge: React Carousel
+// Last Updated: September 22nd, 2023
+
+// What You will learn :
+// React App
+// React Components
+// JSX
+// Bootstrap
+
+
+// Instructions : Build A React Carousel
+// React Carousel
+
+// Check the demo http://di-react-carousel.surge.sh/
+
+
+
+// You will use the React Responsive Carousel Package to create the carousel. https://www.npmjs.com/package/react-responsive-carousel
+
+// In your App component make sure to use the corresponding imports of the installed package
+
+// Click on the links below, to retrieve the images for your carousel:
+// Hong Kong https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/jrfyzvgzvhs1iylduuhj.jpg
+// , Macao https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/c1cklkyp6ms02tougufx.webp
+// , Japan https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/e8fnw35p6zgusq218foj.webp
+// , Las Vegas https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/liw377az16sxmp9a6ylg.webp
+
+//app.js
+// import React from 'react';
+// import { Carousel } from 'react-responsive-carousel';
+// import 'react-responsive-carousel/lib/styles/carousel.min.css'; 
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>React Carousel</h1>
+//       <Carousel>
+//         <div>
+//           <img
+//             src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/jrfyzvgzvhs1iylduuhj.jpg"
+//             alt="Hong Kong"
+//           />
+//           <p className="legend">Hong Kong</p>
+//         </div>
+//         <div>
+//           <img
+//             src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/c1cklkyp6ms02tougufx.webp"
+//             alt="Macao"
+//           />
+//           <p className="legend">Macao</p>
+//         </div>
+//         <div>
+//           <img
+//             src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/e8fnw35p6zgusq218foj.webp"
+//             alt="Japan"
+//           />
+//           <p className="legend">Japan</p>
+//         </div>
+//         <div>
+//           <img
+//             src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/liw377az16sxmp9a6ylg.webp"
+//             alt="Las Vegas"
+//           />
+//           <p className="legend">Las Vegas</p>
+//         </div>
+//       </Carousel>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// .carousel img {
+//     max-width: 50%; 
+//     height: auto; 
+//     margin: 0 auto; 
+//   }
+  
