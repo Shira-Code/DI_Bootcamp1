80497e201dfcd7bd2ee9e9d3b563e8b44480be3f
Shira-Code <Shira.code@gmail.com>
2024-09-24 07:44:19 +0300

    w8d2 challenge

--- Week8/Day2/day2/src/app.jsx
+++ Week8/Day2/day2/src/app.jsx
@@ -1,13 +1,46 @@
-import React from 'react';
-import Color from './Components/Color.jsx'; 
+// import React from 'react';
+// import Color from './Components/Color.jsx'; 
 
-const App = () => {
-    return (
-        <div>
-            <h1>My React App</h1>
-            <Color /> {}
-        </div>
-    );
-};
+// const App = () => {
+//     return (
+//         <div>
+//             <h1>My React App</h1>
+//             <Color /> {}
+//         </div>
+//     );
+// };
+
+// export default App;
+import React, { useState } from 'react';
+import './Components/styles.css';  
+
+function App() {
+  const [languages, setLanguages] = useState([
+    { name: "Php", votes: 0 },
+    { name: "Python", votes: 0 },
+    { name: "JavaScript", votes: 0 },
+    { name: "Java", votes: 0 }
+  ]);
+
+  const addVote = (index) => {
+    const newLanguages = [...languages];
+    newLanguages[index].votes += 1;
+    setLanguages(newLanguages);
+  };
+
+  return (
+    <div className="container">
+      <h1>Vote for your favorite language</h1>
+      <ul>
+        {languages.map((language, index) => (
+          <li key={index}>
+            <span>{language.name}: {language.votes} votes</span>
+            <button onClick={() => addVote(index)}>Vote</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 
 export default App;
